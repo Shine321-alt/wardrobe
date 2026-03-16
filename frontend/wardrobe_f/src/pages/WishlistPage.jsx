71bfe30e3da0afbf1cd2d4ca0e86be3c2d29693f
shine <pattanadech.m@ku.th>
2026-03-16 17:22:02 +0700

    fix

--- frontend/wardrobe_f/src/pages/WishlistPage.jsx
+++ frontend/wardrobe_f/src/pages/WishlistPage.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import { useAuth } from '../context/AuthContext'
 import { Heart } from 'lucide-react'
-import '../styles/WishlistPage.css'
+import '../styles/Wishlistpage.css'
 
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
 
