c92592c39140cc60043be335667c938073e3390b
krairawee <krairawee.b@live.ku.th>
2026-03-29 03:22:59 +0700

    FIX order detail

--- backend/app.py
+++ backend/app.py
@@ -12,6 +12,7 @@ from routes.shipping import shipping_bp
 from routes.user import user_bp
 from routes.cards import cards_bp
 from routes.checkout import checkout_bp
+from routes.Orders import orders_bp
 
 
 app = Flask(__name__)
@@ -41,6 +42,7 @@ app.register_blueprint(user_bp,      url_prefix='/api')
 app.register_blueprint(cards_bp,     url_prefix='/api')
 app.register_blueprint(shipping_bp,  url_prefix='/api')
 app.register_blueprint(checkout_bp,  url_prefix='/api')
+app.register_blueprint(orders_bp,      url_prefix="/api")
 
 
 
